c057028823ed67063d3d5dd3e72e7e4b0c7a6445
Alexander Deider <arth.inbox@gmail.com>
2014-05-28 13:55:48 +0700

    Processes: forking

--- info.c
+++ info.c
@@ -1,7 +1,19 @@
 #include <stdio.h> /* printf */
+#include <sys/wait.h>
 
 int main(int argc, char const *argv[])
 {
-  printf("Process ID: %d\n", getpid());
+  pid_t pid;
+  printf("%d\tBefore fork\n", getpid());
+  if ((pid = fork()) < 0) {
+    fprintf(stderr, "fork() error\n");
+  } else if(pid == 0) {
+    //child branch
+    printf("%d\tAfter fork\n", getpid());
+  } else {
+    //parent branch
+    printf("%d\tAfter fork\n", getpid());
+    printf("%d\tChild pid: %d\n", getpid(), pid);
+  }
   return 0;
 }
